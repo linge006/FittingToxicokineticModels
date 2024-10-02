309608df1c753f6b94dabad79e7ed571370ef3a4
linge006 <83886629+linge006@users.noreply.github.com>
2024-10-02 15:37:16 +0200

    Update Organism_ToxKin.R

--- Organism_ToxKin.R
+++ Organism_ToxKin.R
@@ -8,43 +8,25 @@ library(ggplot2)
 
 ### Read data of interest (make sure one '#' is removed)
 #dataset <- "Gpulex"
-#dataset <- "ChRiparius"
-#dataset <- "Fcandida_Cu01"
-dataset <- "Fcandida_Cu02"
-#dataset <- "Fcandida_Cd01"
-#dataset <- "Fcandida_Cd02"
-#dataset <- "Fcandida_Cd03"
-#dataset <- "simul"
+#dataset <- "Fcandida_Cu02"
+#dataset <- "sim_homo_full"
+#dataset <- "sim_homo_red"
+#dataset <- "sim_hete_full"
+#dataset <- "sim_hete_red"
 
 # Read data set of choice along with nls model fit starting values, toxicant name and time at which elimination phase starts
 if (dataset=="Gpulex") {t_scale <- "_h"; toxkin_dat <- read.csv(paste0("Gpulex_propanolol_vs",t_scale,".csv"), sep=";", header=T)
   start_nls <- list(C_0=30, k_1=0.618, k_2=0.037); toxic <- "Propanolol"; t_d <- 48}
-if (dataset=="ChRiparius") {toxkin_dat <- read.csv("Ch_riparius_Ag_Silva2023.txt", sep=",", header=T)
-  start_nls <- list(C_0=30, k_1=0.618, k_2=0.037); toxic <- "Ag"; t_scale <- "_h"; t_d <- 48}
-if (dataset=="Fcandida_Cu01") {toxkin_dat <- read.csv("Fcandida_Cu_Ardestani2013_01.txt", sep=",", header=T)
-  start_nls <- list(C_0=44.08, k_1=0.1308, k_2=0.0406); toxic <- "Cu"; t_scale <- "_d"; t_d <- 14}
 if (dataset=="Fcandida_Cu02") {toxkin_dat <- read.csv("Fcandida_Cu_Ardestani2013_02.txt", sep=",", header=T)
   start_nls <- list(C_0=30, k_1=0.618, k_2=0.037); toxic <- "Cu"; t_scale <- "_d"; t_d <- 14}
-if (dataset=="Fcandida_Cd01") {toxkin_dat <- read.csv("Fcandida_Cd_Ardestani2013_01.txt", sep=",", header=T)
-  start_nls <- list(C_0=1, k_1=0.618, k_2=0.037); toxic <- "Cd"; t_scale <- "_d"; t_d <- 21}
-if (dataset=="Fcandida_Cd02") {toxkin_dat <- read.csv("Fcandida_Cd_Ardestani2013_02.txt", sep=",", header=T)
-  start_nls <- list(C_0=1, k_1=0.618, k_2=0.037); toxic <- "Cd"; t_scale <- "_d"; t_d <- 21}
-if (dataset=="Fcandida_Cd03") {toxkin_dat <- read.csv("Fcandida_Cd_Ardestani2013_03.txt", sep=",", header=T)
-  start_nls <- list(C_0=0.07, k_1=0.408, k_2=0.22); toxic <- "Cd"; t_scale <- "_d"; t_d <- 21}
 
+if (grepl("sim", dataset)) {start_nls <- list(C_0=1, k_1=0.618, k_2=0.037); toxic <- "Toxicant"; t_scale <- "_d"}
+if (dataset=="sim_homo_full") toxkin_dat <- read.csv("simul_toxkin_2_homo_full.csv", header=T)
+if (dataset=="sim_homo_red") toxkin_dat <- read.csv("simul_toxkin_2_homo_red.csv", header=T)
+if (dataset=="sim_hete_full") toxkin_dat <- read.csv("simul_toxkin_2_hete_full.csv", header=T)
+if (dataset=="sim_hete_red") toxkin_dat <- read.csv("simul_toxkin_2_hete_red.csv", header=T)
 
-if (dataset=="simul") {
-  t_d <- 14; start_nls <- list(C_0=1, k_1=0.618, k_2=0.037); toxic <- "Toxicant"; t_scale <- "_d";
-  n_row <- 28; #set.seed(95616)
-  #set.seed(222222)
-  toxkin_dat <- data.frame(Time=0:n_row, k_1=runif(n_row+1,0.5,1.5), k_2=runif(n_row+1,0.05,0.15), 
-                         C_exp=rep(1,n_row+1), t_d=rep(t_d,n_row+1), error=runif(n_row+1,-2,2))
-  #toxkin_dat <- data.frame(toxkin_dat, toxicant=with(toxkin_dat, one_compartment(1, k_1, k_2, C_exp, Time, t_d) + error)) 
-  toxkin_dat <- data.frame(toxkin_dat, pred_toxicant=with(toxkin_dat, one_compartment(2, 5, 1, 1, Time, t_d) )) # C_0=1, k_1=1, k_2=0.1, C_exp=1
-  toxkin_dat <- data.frame(toxkin_dat, toxicant=with(toxkin_dat, one_compartment(2, 5, 1, 1, Time, t_d) + 
-                                                       rnorm(nrow(toxkin_dat), 0, pred_toxicant*0.2) ))
-} # End IFs()
-
+# Run "simul_ToxKin.R" for regenerating simulated data 
 
 # Plot data
 plot(toxicant~Time, data=toxkin_dat, pch=20, ylim=c(0,max(toxkin_dat$toxicant)), xlim=c(0,max(toxkin_dat$Time)))
